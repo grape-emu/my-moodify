795c78f82acd5eaf377fbe77eeef90383d685be7
Sher-Min Yang <sherminyang@gmail.com>
2019-01-28 17:56:48 -0500

    feat: rendering single-track view on request

--- client/src/RecommendationsView.js
+++ client/src/RecommendationsView.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
 const RecommendationsView = props => {
-	console.log(props);
 	return (
 		<div>
 			{
-				<iframe
-					id="iFrame"
-					title="myTitle"
-					sandbox="allow-scripts allow-same-origin"
-					src={`https://open.spotify.com/embed/track/${props.id}`}
-					width="300"
-					height="380"
-					frameBorder="0"
-					allow="encrypted-media"
-				/>
+				<div>
+					<iframe
+						id="iFrame"
+						title="myTitle"
+						sandbox="allow-scripts allow-same-origin"
+						src={`https://open.spotify.com/embed/track/${props.id}`}
+						width="300"
+						height="380"
+						frameBorder="0"
+						allow="encrypted-media"
+					/>
+				</div>
 			}
 		</div>
 	);
