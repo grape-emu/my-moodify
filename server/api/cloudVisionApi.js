925e0b2933bf5ef71cea7134b87f19ff70919108
Guli <gulikholmatova@gmail.com>
2019-01-29 11:54:48 -0500

    minor changes to cloud vision api file

--- server/api/cloudVisionApi.js
+++ server/api/cloudVisionApi.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const vision = require('@google-cloud/vision');
 const GoogleAPIKey = './server/api/keys/GoogleAPIKey.json';
-const imageUrl = './client/public/guliSad.jpeg';
+// const imageUrl = './client/public/guliSad.jpeg';
+const imageUrl =
+  'https://s3.amazonaws.com/my-moodify/bucketFolder/1548708223687-lg.jpg';
 
 // Imports the Google Cloud client library &
 // Creates a client
@@ -12,7 +14,7 @@ const client = new vision.ImageAnnotatorClient({
 async function detectFaces(inputFile) {
   try {
     // Make a call to the Vision API to detect the faces
-    const request = { image: { source: { filename: inputFile } } };
+    const request = { image: { source: { imageUri: inputFile } } };
     const response = await client.faceDetection(request);
     const facialData = response[0].faceAnnotations[0];
     console.log('results[0].faceAnnotations[0]', facialData);
