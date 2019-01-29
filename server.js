92faa53e5cbda843befae7209bab4239786b5ae6
Guli <gulikholmatova@gmail.com>
2019-01-29 17:02:05 -0500

    fixed an issue with api/s3 route

--- server.js
+++ server.js
@@ -8,57 +8,57 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
-	res.send({ express: 'Hello From Express' });
+  res.send({ express: 'Hello From Express' });
 });
 app.post('/api/world', (req, res) => {
-	console.log(req.body);
-	res.send(
-		`I received your POST request. This is what you sent me: ${req.body.post}`
-	);
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`
+  );
 });
 
-app.use('/S3', require('./server/api/s3-visionApi.js'));
+app.use('/api/S3', require('./server/api/s3-visionApi.js'));
 const functionConversion = 'seed_genres=blues&max_valence=0.5';
 
 function spotifyAPI(params, token) {
-	return new Promise((resolve, reject) => {
-		let options = {
-			hostname: 'api.spotify.com',
-			port: 443,
-			path: '/v1/' + params,
-			method: 'GET',
-			headers: {
-				Authorization: ' Bearer ' + token
-			}
-		};
+  return new Promise((resolve, reject) => {
+    let options = {
+      hostname: 'api.spotify.com',
+      port: 443,
+      path: '/v1/' + params,
+      method: 'GET',
+      headers: {
+        Authorization: ' Bearer ' + token,
+      },
+    };
 
-		let httpsObject = https.get(options, res => {
-			let data = '';
-			res
-				.on('data', chunk => {
-					data += chunk;
-				})
-				.on('end', () => {
-					resolve(data);
-				})
-				.on('error', err => {
-					reject(err);
-				});
-		});
-	});
+    let httpsObject = https.get(options, res => {
+      let data = '';
+      res
+        .on('data', chunk => {
+          data += chunk;
+        })
+        .on('end', () => {
+          resolve(data);
+        })
+        .on('error', err => {
+          reject(err);
+        });
+    });
+  });
 }
 
 app.get('/api/spotify/find', async (req, res, next) => {
-	let token = req.query.token;
-	try {
-		const data = await spotifyAPI(
-			`recommendations?${functionConversion}`,
-			token
-		);
-		res.json(JSON.parse(data));
-	} catch (err) {
-		next(err);
-	}
+  let token = req.query.token;
+  try {
+    const data = await spotifyAPI(
+      `recommendations?${functionConversion}`,
+      token
+    );
+    res.json(JSON.parse(data));
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
