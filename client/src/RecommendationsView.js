060eda5729eb4d3948edf1fdf04bf365a12fb864
Sher-Min Yang <sherminyang@gmail.com>
2019-01-29 12:21:49 -0500

    implemente embedded player button

--- client/src/RecommendationsView.js
+++ client/src/RecommendationsView.js
@@ -11,10 +11,11 @@ const RecommendationsView = props => {
 						sandbox="allow-scripts allow-same-origin"
 						src={`https://open.spotify.com/embed/track/${props.id}`}
 						width="300"
-						height="380"
+						height="80"
 						frameBorder="0"
 						allow="encrypted-media"
 					/>
+			
 				</div>
 			}
 		</div>
