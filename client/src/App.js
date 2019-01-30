8fb6930798416b1e82be469a3d0e44c703520646
Guli <gulikholmatova@gmail.com>
2019-01-30 12:58:45 -0500

    deleted Form from the main view as it was integrated into another component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Navbar from './navbar';
 import './App.css';
 import SpotifyDisplay from './SpotifyComponents/spotify-display';
-import ImageForm from './ImageForm.js';
 
 class App extends Component {
   render() {
