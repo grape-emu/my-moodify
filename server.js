8c4a43310826af05ba49f3c73f32e160d88e1cf5
Elcess <kelcess@alum.mit.edu>
2019-01-30 15:19:08 -0500

    remove build artifacts

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const path = require('path');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', require('./server/api'));
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build/index.html'));
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
