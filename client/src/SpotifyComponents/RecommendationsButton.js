607e4d2c1fbdff34240b49eb545cfc15cc597f75
Guli <gulikholmatova@gmail.com>
2019-01-30 12:45:38 -0500

    integrated s3 functionality into another component

--- client/src/SpotifyComponents/RecommendationsButton.js
+++ client/src/SpotifyComponents/RecommendationsButton.js
@@ -6,44 +6,77 @@ import RecommendationsView from './RecommendationsView';
 const convertedFromFunction = '?seed_genres=blues&max_valence=0.5';
 
 export default class RecommendationsButton extends Component {
-	constructor() {
-		super();
-		this.state = {
-			tracks: []
-		};
-		this.handleRecommendations = this.handleRecommendations.bind(this);
-	}
-	handleRecommendations = async () => {
-		try {
-			const token = getHashParams();
+  constructor() {
+    super();
+    this.state = {
+      tracks: [],
+      file: null,
+    };
+    this.handleRecommendations = this.handleRecommendations.bind(this);
+  }
 
-			//make request to backend to fetch querry from Spotify API
-			//using relative path because of proxy setup for create-react-app
-			const { data } = await axios.get(
-				`/api/spotify/find?token=${
-					token.access_token
-				}&recommendations=${convertedFromFunction}`
-			);
-			//update local state with URL
-			this.setState({
-				tracks: data.tracks
-			});
-		} catch (err) {
-			console.error(err);
-		}
-	};
-	render() {
-		return (
-			<div>
-				<button type="button" onClick={this.handleRecommendations}>
-					Get Recommendations
-				</button>
-				{this.state.tracks.length === 0 ? (
-					<div />
-				) : (
-					<RecommendationsView tracks={this.state.tracks} />
-				)}
-			</div>
-		);
-	}
+  handleRecommendations = async () => {
+    try {
+      const token = getHashParams();
+
+      //make request to backend to fetch querry from Spotify API
+      //using relative path because of proxy setup for create-react-app
+      const { data } = await axios.get(
+        `/api/spotify/find?token=${
+          token.access_token
+        }&recommendations=${convertedFromFunction}`
+      );
+      //update local state with URL
+      this.setState({
+        tracks: data.tracks,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  submitFile = event => {
+    console.log('submitFile working...');
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', this.state.file[0]);
+    axios
+      .post('/api/s3/test-upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(result => console.log('result.data,', result.data))
+      .catch(error => {
+        console.log('error');
+      });
+  };
+
+  handleFileUpload = event => {
+    console.log('updating');
+    this.setState({ file: event.target.files });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitFile}>
+          <input
+            label="upload file"
+            type="file"
+            onChange={this.handleFileUpload}
+          />
+          <button type="submit">Send</button>
+        </form>
+
+        <button type="button" onClick={this.handleRecommendations}>
+          Get Recommendations
+        </button>
+        {this.state.tracks.length === 0 ? (
+          <div />
+        ) : (
+          <RecommendationsView tracks={this.state.tracks} />
+        )}
+      </div>
+    );
+  }
 }
