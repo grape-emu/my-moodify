3af2e82b0385893bc7b457d05018be39774a4b1b
Claire Gilligan <feminaprovita@gmail.com>
2019-01-31 11:37:13 -0500

    bug: renamed /api/spotify to /api/spotify-tracks

--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 module.exports = router;
 
 // router.use('/s3', require('./s3-visionApi.js'));
-router.use('/spotify', require('./spotify'));
+router.use('/spotify', require('./spotify-tracks'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
