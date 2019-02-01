fe5457ebf2b95fb708f26b0a5371e9db03835510
Claire Gilligan <feminaprovita@gmail.com>
2019-02-01 13:15:05 -0500

    chore: cleaned up server/api/conversionFunction after linter changed line numbers

--- server/api/conversionFunction.js
+++ server/api/conversionFunction.js
@@ -1,4 +1,4 @@
-// Begin at convertGoogleCloudVisionObjToSpotifyString (line 116)
+// Begin at convertGoogleCloudVisionObjToSpotifyString (line 128)
 
 class SpotifyUrlObject {
   // eslint-disable-next-line max-params
@@ -8,7 +8,7 @@ class SpotifyUrlObject {
     sorrowScale,
     surpriseScale,
     angerScale,
-    radius = 0.4,
+    diameter = 0.4,
     minVal = 0,
     maxVal = 1
   ) {
@@ -19,13 +19,13 @@ class SpotifyUrlObject {
     this.sorrowScale = sorrowScale;
     this.surpriseScale = surpriseScale;
     this.angerScale = angerScale;
-    // In this.printString, we use a fixed point radius to give spotify two values (min and max) for each tag; this tells the function how big that range should be
-    this.radius = radius;
-    // Most tags are ranged from 0-1, but some have other values, thus the defaults (for these and also this.radius)
+    // In this.printString, we use a fixed point radius to give spotify two values (min and max) for each tag; this tells the function the diameter of that range
+    this.diameter = diameter;
+    // Most tags are ranged from 0-1, but some have other values, thus the defaults (for these and also this.diameter)
     this.minVal = minVal;
     this.maxVal = maxVal;
   }
-  // Line 22 maps over the array of instances with this.addEmotionKeys, just below
+  // Line 30 maps over the array of instances with this.addEmotionKeys, just below
 
   addEmotionKeys(obj) {
     // Then we take our selfieObj (from Google Cloud Vision) and add numeric values to our instance that correspond to the emotion values the selfieObj contains (like in ../../conversion/spotifyObjEmotions.js)
@@ -39,7 +39,7 @@ class SpotifyUrlObject {
     }
     return this;
   }
-  // Return to line 135, which maps over the output of this method with this.printString, line 70
+  // Return to line 165, which maps over the output of this method with this.printString, line 79
 
   translator() {
     // rangeWidth holds the largest possible output range, based on what spotify permits
@@ -60,7 +60,7 @@ class SpotifyUrlObject {
       emotionTotal += this.sorrowLikelihood * this.sorrowScale;
       emotionTotal += this.surpriseLikelihood * this.surpriseScale;
       emotionTotal += this.angerLikelihood * this.angerScale;
-      /* The output below (line 63) marks the single point that maps most closely to the emotions Google Cloud Vision detected in the selfie.
+      /* The output below (line 70) marks the single point that maps most closely to the emotions Google Cloud Vision detected in the selfie.
       Here is a more granular rewrite of what the range function below is doing:
           const emotionTotalAbsoluteValue = emotionTotal - (-1 * scaledMax);
           const emotionTotalPercent = emotionTotalAbsoluteValue / (scaledMax * 2);
@@ -74,20 +74,20 @@ class SpotifyUrlObject {
     };
     return fixedPoint;
   }
-  // Return to line 73
+  // Return to line 82
 
   printString() {
-    // this.translator (line 37) will yield the midpoint for our fixed point radius
-    // note that we invoke it immediately, so we actually return the output of fixedPoint
+    // this.translator (line 44) will yield the midpoint for our fixed point radius
+    // Note that we invoke it immediately, so we actually return the output of fixedPoint
     this.midpoint = this.translator()();
     const cheapRound = x => Math.round(x * 100000) / 100000;
     /* rangeMin and rangeMax are the ACTUAL limits of our measurement, based on emotion and scale
-    as opposed to this.minVal and this.maxVal, which are the outer POSSIBLE limits of the range */
+    As opposed to this.minVal and this.maxVal, which are the outer POSSIBLE limits of the range */
     const rangeMin = cheapRound(
-      Math.max(this.midpoint - this.radius / 2, this.minVal)
+      Math.max(this.midpoint - this.diameter / 2, this.minVal)
     );
     const rangeMax = cheapRound(
-      Math.min(this.midpoint + this.radius / 2, this.maxVal)
+      Math.min(this.midpoint + this.diameter / 2, this.maxVal)
     );
     // Genre comes first in the string, and also follows some different output rules
     if (this.name === 'genre') {
@@ -106,26 +106,20 @@ class SpotifyUrlObject {
     }
   }
 }
-// Return to line 139
+// Return to line 171
 
-// For each spotify key that we intend to use, we need to initialize an instance of the constructor (see details begin at line 7)
+// For each spotify key that we intend to use, we need to initialize an instance of the constructor (see details begin at line 16)
 const genreUrlObj = new SpotifyUrlObject('genre', 2, -2, 0, 0);
 const modeUrlObj = new SpotifyUrlObject('mode', 2, -2, 0, 0);
 const valenceUrlObj = new SpotifyUrlObject('valence', 2, -2, 1, -1);
 const energyUrlObj = new SpotifyUrlObject('energy', 2, -1.5, 0, 1);
-// const acousticnessUrlObj = new SpotifyUrlObject('acousticness',0,1,0,-1,0.33);
-// const danceabilityUrlObj = new SpotifyUrlObject('danceability',1,-1,1,0);
-// const instrumentalnessUrlObj = new SpotifyUrlObject('instrumentalness',-1,0,-1,-1);
-// const popularityUrlObj = new SpotifyUrlObject('popularity',1,0,0,-1);
-// const loudnessUrlObj = new SpotifyUrlObject('loudness',0,-1,0,2,30,-60,1);
-// const tempoUrlObj = new SpotifyUrlObject('tempo',1,-2,-1,2,100,50,210);
 
-// Before we can act, we need an array that contains the instances for all the spotify keys we care about (see just above, line 99)
+// Before we can act, we need an array that contains the instances for all the spotify keys we care about (see just above, line 112)
 const fullUrlObject = [
   genreUrlObj,
   modeUrlObj,
   valenceUrlObj,
-  energyUrlObj /*, acousticnessUrlObj, danceabilityUrlObj, instrumentalnessUrlObj, popularityUrlObj, loudnessUrlObj, tempoUrlObj*/,
+  energyUrlObj
 ];
 
 // *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*
@@ -166,14 +160,14 @@ const convertGoogleCloudVisionObjToSpotifyString = selfieObj => {
     return 'error';
   }
 
-  // Before we can process the selfie's emotions, we need to initialize our instance of the constructor (fullUrlObject, line 111), which will look a lot like the one in ../../conversion/spotifyObjDefault.js
-  // Then we run addEmotionKeys (line 22) to customize the instance with Google's emotion analysis data
+  // Before we can process the selfie's emotions, we need to initialize our instance of the constructor (fullUrlObject, line 118), which will look a lot like the one in ../../conversion/spotifyObjDefault.js
+  // Then we run addEmotionKeys (line 30) to customize the instance with Google's emotion analysis data
   const specificPhotoObject = fullUrlObject.map(el =>
     el.addEmotionKeys(selfieObj)
   );
   /* now that we have all the data we need in the object, it's time to get the url string spotify needs
-    note that this.printString returns the string for each key in turn, so they need to be joined
-    then these four things are static to every query, so we concat them onto the end */
+    Note that this.printString returns the string for each key in turn, so they need to be joined
+    Then these four things are static to every query, so we concat them onto the end */
   let urlString =
     specificPhotoObject.map(el => el.printString()).join('') +
     '&max_liveness=0.75&max_speechiness=0.66&market=US&explicit=false';
@@ -181,7 +175,7 @@ const convertGoogleCloudVisionObjToSpotifyString = selfieObj => {
   return urlString;
 };
 
-// Retaining this for easy testing purposes
+// Retaining this for easy testing purposes:
 // console.log(convertGoogleCloudVisionObjToSpotifyString({
 //   joyLikelihood: 'VERY_UNLIKELY',
 //   sorrowLikelihood: 'VERY_LIKELY',
@@ -189,4 +183,3 @@ const convertGoogleCloudVisionObjToSpotifyString = selfieObj => {
 //   surpriseLikelihood: 'VERY_UNLIKELY'}))
 
 module.exports = convertGoogleCloudVisionObjToSpotifyString;
-// adding extra comment so that github will recognize this as a changed file and commit my changes
