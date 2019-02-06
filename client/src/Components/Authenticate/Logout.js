3bbaf52c86c23e85916daa6b047491f4e09c7ff4
Elcess <kelcess@alum.mit.edu>
2019-02-06 14:40:00 -0500

    add logout button

---
+++ client/src/Components/Authenticate/Logout.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#6200ea' }, // deepPurple[a700]
+    secondary: { main: '#3f51b5' }, // indigo[500]
+    accent: { main: '#2196f3' }, // blue[500]
+  },
+  typography: { useNextVariants: true },
+});
+
+class Logout extends Component {
+  constructor() {
+    super();
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout(e) {
+    e.preventDefault();
+    window.open('https://accounts.spotify.com/logout');
+    window.location.replace('/');
+  }
+
+  render() {
+    return (
+      <div className="logout">
+        <MuiThemeProvider theme={theme}>
+          <Button
+            variant="contained"
+            type="button"
+            color="primary"
+            onClick={this.handleLogout}
+          >
+            Logout from Spotify
+          </Button>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+export default Logout;
--- client/src/Components/MainDisplay.js
+++ client/src/Components/MainDisplay.js
@@ -1,57 +1,64 @@
 import React, { Component } from 'react';
 import { Navbar, SelectionDisplay } from './index';
 import Button from '@material-ui/core/Button';
+import Logout from './Authenticate/Logout';
 
 export default class MainDisplay extends Component {
-	constructor() {
-		super();
-		this.state = {
-			option: false
-		};
-		this.handleCapture = this.handleCapture.bind(this);
-		this.handleUpload = this.handleUpload.bind(this);
-		this.handleReset = this.handleReset.bind(this);
-	}
+  constructor() {
+    super();
+    this.state = {
+      option: false,
+    };
+    this.handleCapture = this.handleCapture.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
 
-	handleCapture() {
-		this.setState({ option: 'capture' });
-	}
+  handleCapture() {
+    this.setState({ option: 'capture' });
+  }
 
-	handleUpload() {
-		this.setState({ option: 'upload' });
-	}
+  handleUpload() {
+    this.setState({ option: 'upload' });
+  }
 
-	handleReset() {
-		this.setState({ option: false });
-	}
+  handleReset() {
+    this.setState({ option: false });
+  }
 
-	render() {
-		return (
-			<div>
-				<header className="App-header">
-					<Navbar className="App-header" />
-				</header>
-				{!this.state.option && (
-					<div>
-						<p>Help us understand your mood!</p>
-						<Button type="button" onClick={this.handleUpload}>
-							Upload an image
-						</Button>
-						<Button type="button" onClick={this.handleCapture}>
-							Take a selfie
-						</Button>
-					</div>
-				)}
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <Navbar className="App-header" />
+        </header>
+        {!this.state.option && (
+          <div>
+            <p>Help us understand your mood!</p>
+            <Button type="button" onClick={this.handleUpload}>
+              Upload an image
+            </Button>
+            <Button type="button" onClick={this.handleCapture}>
+              Take a selfie
+            </Button>
+          </div>
+        )}
 
-				{this.state.option && (
-					<div>
-						<SelectionDisplay option={this.state.option} />
-						<Button type="button" onClick={this.handleReset}>
-							Start Over?
-						</Button>
-					</div>
-				)}
-			</div>
-		);
-	}
+        {this.state.option && (
+          <div>
+            <SelectionDisplay option={this.state.option} />
+            <Button type="button" onClick={this.handleReset}>
+              Start Over?
+            </Button>
+          </div>
+        )}
+
+        {this.state.option && (
+          <div>
+            <Logout />
+          </div>
+        )}
+      </div>
+    );
+  }
 }
