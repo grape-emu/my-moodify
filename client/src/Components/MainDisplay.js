c812788e911fe6cf33a2bc399b0d37e08a347a44
Claire Gilligan <feminaprovita@gmail.com>
2019-02-06 17:16:25 -0500

    added dictionary definition

--- client/src/Components/MainDisplay.js
+++ client/src/Components/MainDisplay.js
@@ -69,6 +69,29 @@ export default class MainDisplay extends Component {
                   </Button>
                 </div>
               </MuiThemeProvider>
+
+              <br />
+              <div id="dictionary-entry">
+                <h3>mood·i·fy /ˈmuːdɪˌfaɪ/</h3>
+                <p>
+                  <em>Verb.</em>
+                  <br />
+                  <strong>
+                    To transform an image of a face into a playlist matching
+                    that image's mood.
+                  </strong>
+                </p>
+                <p>
+                  <em>Noun.</em>
+                  <br />
+                  <strong>
+                    Web app that moodifies a selfie or uploaded photo.
+                    <br />
+                    Designed by Claire Gilligan, Guli Kholmatova, Kimberley
+                    Elcess, and Sher-Min Yang in 2019.
+                  </strong>
+                </p>
+              </div>
             </div>
           )}
 
